625007aa779cd74814da8af294c56763e9a9fe77
Sohaib Khan <sohaib5764@gmail.com>
2021-07-24 14:24:00 +0500

    Permission Based Authentication

--- src/main/java/com/spring/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spring/security/security/ApplicationSecurityConfig.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.spring.security.security.ApplicationUserPermissions.*;
 import static com.spring.security.security.ApplicationUserRoles.*;
 
 @Configuration
